1c34c11f1439d6db763a5b292c73721beafeafb3
creaticoding <seokho.jeong@toss.im>
2022-10-23 20:40:52 +0900

    fix: domain and referer [skip vercel] [skip vercel]

--- src/pages/other-domain.tsx
+++ src/pages/other-domain.tsx
@@ -1,16 +1,41 @@
 import { GetServerSideProps } from "next";
 
+const DOMAIN = "https://app.divops.kr";
+
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  console.log(context.req.headers);
+  const referer = getReferer(context.req.headers.host);
+
+  if (referer == null) {
+    return {
+      props: {
+        message: `[500] context.req.headers.host is ${context.req.headers.host}`,
+      },
+    };
+  }
+
   return {
     redirect: {
-      destination:
-        "http://localhost:3000/login?referer=https://app.divops.kr/login/test",
+      destination: `${DOMAIN}/login?referer=${referer}/login/test`,
       permanent: false,
     },
   };
 };
 
-export default function IndexPage() {
-  return <></>;
+export default function IndexPage({ message }: { message: string }) {
+  return <>{message}</>;
+}
+
+function getReferer(host?: string) {
+  if (host == null || host.trim() === "") {
+    return null;
+  }
+
+  switch (host) {
+    case "localhost:3000": {
+      return `http://${host}`;
+    }
+    default: {
+      return `https://${host}`;
+    }
+  }
 }
