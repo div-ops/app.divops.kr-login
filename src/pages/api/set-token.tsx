babd78ed63fe5a37117d4ed6501bd80c8d13e135
정석호 <seokho.jeong@toss.im>
2022-12-18 16:44:56 +0900

    fix [skip vercel] [skip vercel]

--- src/pages/api/set-token.tsx
+++ src/pages/api/set-token.tsx
@@ -1,9 +1,17 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import NextCors from "nextjs-cors";
 
 export default async function SetToken(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  await NextCors(req, res, {
+    // Options
+    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+    origin: ["https://www.creco.services", "http://localhost:3000"],
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  });
+
   const code = req.body?.code;
 
   const decoded = decodeURIComponent(code);
